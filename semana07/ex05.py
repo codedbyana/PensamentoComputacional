418ab45f8c957ef58daae9eb9656d1600f2959aa
Ana Luiza :) <anallu.rochas@gmail.com>
2025-09-21 16:48:33 -0300

    Create ex05.py

---
+++ semana07/ex05.py
@@ -0,0 +1,9 @@
+
+#Solicite ao usuário um número inteiro positivo e mostre a sequência de
+# números decrescentes de N até 1.
+
+n = int(input('digite um numero inteiro:'))
+
+while n >= 1:
+    print(n)
+    n -= 1
