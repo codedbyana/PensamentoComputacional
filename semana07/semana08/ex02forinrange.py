9492e693d0e1477cd16047d5b706a2c6cd26efa9
Ana Luiza :) <anallu.rochas@gmail.com>
2025-10-08 21:11:53 -0300

    Update ex02forinrange.py

--- semana07/semana08/ex02forinrange.py
+++ semana07/semana08/ex02forinrange.py
@@ -1,1 +1,11 @@
 #Faça um algoritmo que peça dois números para o usuário (o primeiro sempre será menor que o segundo), posteriormente apresente somente os números pares no intervalo entre os dois número. 
+while True:
+    n1 = int(input("digite o primeiro numero (deve ser baixo):"))
+    n2 = int(input("Digite o segundo número:"))
+   
+    for i in range (n1, n2):
+        if i % 2 == 0:
+            print("Numeros pares", i )
+    else:
+     print("Valor 1 deve ser menor")
+        
