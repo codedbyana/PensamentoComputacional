84d4d4dae8d9d743f9b463c9233a6b02285e61f3
Ana Luiza :) <anallu.rochas@gmail.com>
2025-10-08 21:05:30 -0300

    Create ex02forinrange.py

---
+++ semana07/semana08/ex02forinrange.py
@@ -0,0 +1,1 @@
+#Faça um algoritmo que peça dois números para o usuário (o primeiro sempre será menor que o segundo), posteriormente apresente somente os números pares no intervalo entre os dois número. 
