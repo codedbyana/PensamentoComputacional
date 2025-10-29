d5ab9d02367c2017b1e08f0ae95bdd6f7af112cf
Ana Luiza :) <anallu.rochas@gmail.com>
2025-10-29 18:31:51 -0300

    Add functions to calculate double and triple of a number

---
+++ semana10/ex03.py
@@ -0,0 +1,11 @@
+
+def dobro(valor):
+    return valor * 2
+
+def triplo(valor):
+    return valor * 3
+
+# Programa principal
+num = float(input("Digite um número: "))
+print(f"Dobro: {dobro(num)}")
+print(f"Triplo: {triplo(num)}")
