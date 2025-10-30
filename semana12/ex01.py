c5d2937d1c7aae0cfc854aaffad5feee1a8c9f9c
Ana Luiza :) <anallu.rochas@gmail.com>
2025-10-30 15:53:02 -0300

    Add animal list and modify second element

---
+++ semana12/ex01.py
@@ -0,0 +1,8 @@
+
+animais = ['cachorro', 'gato', 'papagaio', 'tartaruga']
+print("Primeiro animal:", animais[0])
+print("Último animal:", animais[-1])
+
+# Modificando o segundo elemento
+animais[1] = 'coelho'
+print("Lista modificada:", animais)
